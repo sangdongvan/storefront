17dcce09c76ec28e921eaedb5d42ddc2e1d4acd3
Sang Dong <sang.dongvan@asnet.com.vn>
2024-12-05 12:45:41 +0700

    fix config headers

--- apps/webapp/app/routes/api/get-app-config.server.ts
+++ apps/webapp/app/routes/api/get-app-config.server.ts
@@ -3,10 +3,6 @@ import invariant from "tiny-invariant";
 import { AppConfig } from "~/config";
 import { isAppContext } from "~/context.server";
 
-export const headers = {
-  "Cache-Control": "public, max-age=30, s-max-age=3600",
-};
-
 export const loader: LoaderFunction = async ({
   context,
 }: LoaderFunctionArgs) => {
@@ -14,5 +10,9 @@ export const loader: LoaderFunction = async ({
   const config: AppConfig = {
     storefrontApi: context.config.storefrontApi,
   };
-  return Response.json(config);
+  return Response.json(config, {
+    headers: {
+      "Cache-Control": "public, max-age=30, s-max-age=3600",
+    },
+  });
 };
