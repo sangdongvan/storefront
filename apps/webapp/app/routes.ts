c48bd6ec0c6ce94d8a78a082a259380ca885264a
Sang Dong <sang.dongvan@asnet.com.vn>
2024-12-05 11:29:19 +0700

    use layout

--- apps/webapp/app/routes.ts
+++ apps/webapp/app/routes.ts
@@ -1,17 +1,21 @@
 import {
   type RouteConfig,
   index,
+  layout,
+  prefix,
   route,
 } from "@react-router/dev/routes";
 
 export default [
   index("./routes/home.tsx"),
   route("login", "./routes/login.tsx"),
-  route("contacts", "./routes/contacts/layout.tsx", [
-    index("./routes/contacts/list.tsx"),
-    route(":id", "./routes/contacts/view.tsx"),
-    route(":id/edit", "./routes/contacts/edit.tsx"),
-    route(":id/destroy", "./routes/contacts/destroy.tsx"),
+  layout("./routes/contacts/layout.tsx", [
+    ...prefix("contacts", [
+      index("./routes/contacts/list.tsx"),
+      route(":id", "./routes/contacts/view.tsx"),
+      route(":id/edit", "./routes/contacts/edit.tsx"),
+      route(":id/destroy", "./routes/contacts/destroy.tsx"),
+    ]),
   ]),
   route("api/get-access-token", "./routes/api/get-access-token.server.ts"),
   route("api/get-app-config", "./routes/api/get-app-config.server.ts"),
