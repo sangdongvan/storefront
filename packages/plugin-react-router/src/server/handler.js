d40ad8184048d51c13187eca738505d1b978ae75
Sang Dong <sang.dongvan@asnet.com.vn>
2024-12-09 14:46:03 +0700

    remove unused

--- packages/plugin-react-router/src/server/handler.js
+++
@@ -1,7 +0,0 @@
-const { createRequestHandler } = import("@react-router/architect");
-
-exports.handler = createRequestHandler({
-  // @ts-expect-error - virtual module provided by React Router at build time
-  build: import("./build"),
-  mode: process.env.NODE_ENV || "development",
-});
